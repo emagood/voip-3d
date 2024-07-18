cc0514d63b04adf1e8a5ae5cf8996037db04dbdc
ogyrec <ogyrec.off@gmail.com>
2024-07-18 20:43:13 +0200

    Initial commit

---
+++ world.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+const playerScene = preload("res://character.tscn")
+
+func _ready():
+	if multiplayer.is_server():
+		push_warning("SERVER SESSION")
+		multiplayer.peer_connected.connect(createPlayer)
+		multiplayer.peer_disconnected.connect(removePlayer)
+	else:
+		push_warning("CLIENT SESSION")
+	pass
+
+@onready var playersNode = $players
+func createPlayer(id = 1):
+	print("CREATE PLAYER %s" % id)
+	var player = playerScene.instantiate()
+	player.name = str(id)
+	playersNode.add_child(player, true)
+
+func removePlayer(id = 1):
+	print("Player remove: ", str(id))
+	var player = playersNode.get_node_or_null(str(id))
+	if player:
+		player.queue_free()
